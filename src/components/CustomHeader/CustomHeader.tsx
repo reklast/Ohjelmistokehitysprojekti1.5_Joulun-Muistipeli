5576c89a46397dfc8a10c5a31e49c2e6f8785a95
reklast <reklast51@gmail.com>
2023-12-11 14:28:44 +0200

    best score display added

--- src/components/CustomHeader/CustomHeader.tsx
+++ src/components/CustomHeader/CustomHeader.tsx
@@ -4,13 +4,15 @@ import "./CustomHeader.css";
 
 interface HeaderProps {
   score: number;
+  bestScore: string;
 }
 
-const CustomHeader: React.FC<HeaderProps> = ({ score }) => (
+const CustomHeader: React.FC<HeaderProps> = ({ score, bestScore }) => (
   <Header className="header">
     <h1> Jouluinen muistipeli</h1>
 
-    <p className="score">Score: {score}</p>
+    <p className="score">Current Score: {score}</p>
+    <p className="bestScore">Best Score: {bestScore}</p>
   </Header>
 );
 
