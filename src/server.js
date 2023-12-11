618e1f44d1c69b162f8028fec9b26714020581e0
reklast <reklast51@gmail.com>
2023-12-11 14:23:28 +0200

    server configuration and DB connection

--- src/server.js
+++ src/server.js
@@ -1,23 +1,96 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.REACT_APP_PORT || 5000;
+
+require("dotenv").config();
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/Cluster0", {
+mongoose.connect(process.env.REACT_APP_MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const Item = require("./models/Item"); // Create the Item model
+const playerSchema = require("./models/playerSchema");
 
-app.get("/api/items", async (req, res) => {
+app.use(express.json());
+
+app.get("/items", async (req, res) => {
   try {
-    const items = await Item.find();
+    const items = await playerSchema.find();
     res.json(items);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
   }
 });
+
+app.post("/createPlayer", async (req, res) => {
+  console.log('REQUEST BODY HERE: ' + req.body.name);
+  try {
+    playerSchema.findOne({ Name: req.body.name })
+      .then((data) => {
+        if (!data) {
+          console.log('No data created for this player');
+          playerSchema.create({
+            Name: req.body.name,
+            BestScore: "0",
+          });
+          res.send(`Data has been added for this player`);
+        }
+        if (data) {
+          res.send(`${data} this player already added to the DB`);
+        }
+      })
+  } catch (e) {
+    return console.log(e);
+  }
+})
+
+app.post("/getPlayersScore", async (req, res) => {
+  try {
+    playerSchema.findOne({ Name: req.body.name })
+      .then((data) => {
+        if (!data) {
+          console.log('Creating a new Player instance');
+          playerSchema.create({
+            Name: req.body.name,
+            BestScore: "0",
+          });
+          res.send(`Data has been added for this player`)
+        }
+        if (data) {
+          console.log('Player exists');
+          res.send(JSON.stringify(data.BestScore))
+        }
+      })
+  } catch (e) {
+    return console.log(e);
+  }
+})
+
+app.post("/setBestScore", async (req, res) => {
+  console.log('REQUEST BODY HERE: ' + req.body.name);
+  try {
+    playerSchema.findOne({ Name: req.body.name })
+      .then((data) => {
+        if (!data) {
+          console.log('No data created for this player');
+          playerSchema.create({
+            Name: req.body.name,
+            BestScore: "0",
+          });
+          res.send(`Data has been added for this player`);
+        }
+        if (data) {
+          data.BestScore = req.body.newBestScore
+          data.save();
+          res.send('Best score has been updated');
+        }
+      })
+  } catch (e) {
+    return console.log(e);
+  }
+})
